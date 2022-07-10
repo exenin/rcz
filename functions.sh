f5489e02d32f425e514e759feef1cbea2a04757c
Your Name <you@example.com>
2022-07-09 23:17:47 -0400

    added terraform version manager

--- functions.sh
+++ functions.sh
@@ -10,3 +10,21 @@ function rczsetup(){
 function rczupdate(){
   cd ~/.rcz; git pull
 }
+
+function downloadtf(){
+    v=$1
+    wget https://releases.hashicorp.com/terraform/$v/terraform_${v}_linux_amd64.zip -O /tmp/tf${v}.zip
+    cd $v || mkdir $v && cd $v
+    unzip /tmp/tf${v}.zip
+
+
+}
+function tvm(){
+	ls -las ~/.tvm || mkdir ~/.tvm
+	cd ~/.tvm
+	v=$1
+	ls ~/.tvm/$v/terraform || downloadtf $v
+ 	sudo rm /usr/local/bin/terraform
+
+        sudo ln -s ~/.tvm/$v/terraform /usr/local/bin/terraform
+}
